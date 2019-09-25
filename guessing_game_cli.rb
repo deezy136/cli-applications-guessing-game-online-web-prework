ba650e1319f3bee8eef8ee95fea91283489410a9
deezy13 <deezy13@gmail.com>
2019-09-25 06:11:02 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -9,7 +9,7 @@ def run_guessing_game
   user_input = gets.chomp 
   
   if user_input == random_number
-    puts " You guessed the correct number!"
+    puts "You guessed the correct number!"
     
     elsif user_input != random_number
     puts "Sorry! The computer guessed #{random_number}."
